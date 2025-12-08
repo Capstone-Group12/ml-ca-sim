f64cbd383d7cce204ad3a47abcce28c7f1532cfe
brandonlevack <135759075+brandonlevack@users.noreply.github.com>
2025-12-07 21:26:22 -0500

    Update dos.py for more workers

--- simulations/dos.py
+++ simulations/dos.py
@@ -15,7 +15,7 @@ class dos_attack:
         except Exception as e:
             return f"Error: {e}"
 
-    def run(self, workers=10):                      # In parallel send requests in range i
+    def run(self, workers=200):                      # In parallel send requests in range i
         with ThreadPoolExecutor(max_workers=workers) as executor:
             futures = [executor.submit(self.send_request) for _ in range(self.i)]
             for future in as_completed(futures):
