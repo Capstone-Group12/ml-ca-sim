543c4dd5dba0afeed43609bc2d2b843efe88c3e9
brandonlevack <brandonlevack@hotmail.com>
2025-12-08 11:07:51 -0500

    changes to brute force to set max iterations

--- simulations/brute_force.py
+++ simulations/brute_force.py
@@ -1,13 +1,16 @@
 import requests
+import sys
 
 class brute_force:
-    def __init__(self):
+    def __init__(self, max_iterations):
         self.endpoint = "https://mlcasim-api.edwardnafornita.com"
         self.passwordLength = 5
         self.possibleChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '.', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '<', '>', '/', '?']
         self.successful_password = ""
+        self.max_iterations = max_iterations
 
     def run(self):
+        count = 0
         for a in range(len(self.possibleChars)):
             first_char = self.possibleChars[a]
             for b in range(len(self.possibleChars)):
@@ -28,6 +31,18 @@ class brute_force:
                             response = requests.post(self.endpoint, json=payload)
 
                             print(response)
+                            
+                            count += 1
 
                             if response.status_code == 200:
                                 self.successful_password = password
+                                sys.exit(f"Password discovered: {successful_password}")
+                            
+                            if count >= self.max_iterations:
+                                sys.exit("Completed desired iterations")
+
+'''
+If desired - remove comments
+brute_obj = brute_force(int(sys.argv[1]))
+brute_obj.run()
+'''
